297b3ab68ecf163b1fda28f4b3067354fbb0e115
Aynur Nasybullin <nasybullin.aynur@yandex.ru>
2023-11-22 11:24:31 +0300

    created: ios app project;

---
+++ DartsScore/DartsScore/DartsScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartsScoreApp.swift
+//  DartsScore
+//
+//  Created by Aynur Nasybullin on 2023.11.22.
+//
+
+import SwiftUI
+
+@main
+struct DartsScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
